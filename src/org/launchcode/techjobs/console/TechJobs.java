8b416fab428c7246f89c0000f3dc940150bbb4c7
Julia Carpenter <JCarpenter928@gmail.com>
2017-12-11 20:27:11 -0600

    printing works, but the capitalization thing still needs solving.

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -3,6 +3,7 @@ package org.launchcode.techjobs.console;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.Set;
 
 /**
  * Modified by Julia 11/30/2017
@@ -11,7 +12,7 @@ public class TechJobs {
 
     private static Scanner in = new Scanner(System.in);
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         // Initialize our field map with key/name pairs
         HashMap<String, String> columnChoices = new HashMap<>();
@@ -61,7 +62,7 @@ public class TechJobs {
                 String searchTerm = in.nextLine();
 
                 if (searchField.equals("all")) {
-                    System.out.println("Search all fields not yet implemented.");
+                    printJobs(JobData.findByValue(searchTerm));
                 } else {
                     printJobs(JobData.findByColumnAndValue(searchField, searchTerm));
                 }
@@ -103,14 +104,25 @@ public class TechJobs {
                 validChoice = true;
             }
 
-        } while(!validChoice);
+        } while (!validChoice);
 
         return choiceKeys[choiceIdx];
     }
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-
-        System.out.println("printJobs is not implemented yet");
+        if (someJobs.size() == 0) {
+            System.out.println("We do not have a job matching that description.");
+        } else {
+            System.out.println("*****");
+            for (int i = 0; i < someJobs.size(); i++) {
+                HashMap<String, String> thisJob = (HashMap<String, String>) someJobs.get(i);
+                Set<String> keys = thisJob.keySet();
+                for (String key: keys) {
+                    System.out.println(key + ": " + thisJob.get(key));
+                }
+                System.out.println("*****");
+            }
+        }
     }
 }
